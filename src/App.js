d6fcb7deb3a541c152669c1c5312d98201d7df86
Subham Bharadwaz <subhamsbharadwaz@gmail.com>
2021-07-19 11:23:16 +0530

    working with github api and lifecycle method(componentDidMount)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react'
 import './App.css'
 
+// Import 3rd party packages
+import axios from 'axios'
+
 // Import Components
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users'
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  }
+
+  async componentDidMount() {
+    this.setState({ loading: true })
+
+    const res = await axios.get('https://api.github.com/users')
+
+    this.setState({ users: res.data, loading: false })
+  }
+
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Users />
+          <Users users={this.state.users} loading={this.state.loading} />
         </div>
       </div>
     )
