fe9157705d7052be8aaa8c3df8061d75d442a296
Subham Bharadwaz <subhamsbharadwaz@gmail.com>
2021-07-19 16:42:04 +0530

    func->(Search)

--- src/App.js
+++ src/App.js
@@ -25,12 +25,22 @@ class App extends Component {
     this.setState({ users: res.data, loading: false })
   }
 
+  // Search Github users
+  searchUser = async (text) => {
+    this.setState({ loading: true })
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`,
+    )
+
+    this.setState({ users: res.data.items, loading: false })
+  }
+
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Search />
+          <Search searchUser={this.searchUser} />
           <Users users={this.state.users} loading={this.state.loading} />
         </div>
       </div>
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class Search extends Component {
   state = {
     text: '',
   }
 
+  static propTypes = {
+    searchUser: PropTypes.func.isRequired,
+  }
+
   onSubmit = (e) => {
     e.preventDefault()
+    this.props.searchUser(this.state.text)
+    this.setState({ text: '' })
   }
 
   onChange = (e) => this.setState({ [e.target.name]: e.target.value })
