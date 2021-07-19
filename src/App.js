f8edbf6126a4b2bfe461bb3cab46e94bdf547ce2
Subham Bharadwaz <subhamsbharadwaz@gmail.com>
2021-07-19 16:55:09 +0530

    func->(ClearBtn)

--- src/App.js
+++ src/App.js
@@ -15,16 +15,6 @@ class App extends Component {
     loading: false,
   }
 
-  async componentDidMount() {
-    this.setState({ loading: true })
-
-    const res = await axios.get(
-      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`,
-    )
-
-    this.setState({ users: res.data, loading: false })
-  }
-
   // Search Github users
   searchUser = async (text) => {
     this.setState({ loading: true })
@@ -35,13 +25,21 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false })
   }
 
+  // Clear users from state
+  clearUsers = () => this.setState({ users: [], loading: false })
+
   render() {
+    const { users, loading } = this.state
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Search searchUser={this.searchUser} />
-          <Users users={this.state.users} loading={this.state.loading} />
+          <Search
+            searchUser={this.searchUser}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false}
+          />
+          <Users users={users} loading={loading} />
         </div>
       </div>
     )
