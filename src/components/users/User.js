27aac446bf02dfe888cd8846460b1816f3fda32e
Subham Bharadwaz <subhamsbharadwaz@gmail.com>
2021-07-19 18:56:42 +0530

    displaying repos added markdown

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -6,16 +6,20 @@ import { Link } from 'react-router-dom'
 
 // Import components
 import Spinner from '../layout/Spinner'
+import Repos from '../repos/Repos'
 
 class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login)
+    this.props.getUserRepos(this.props.match.params.login)
   }
 
   static propTypes = {
     loading: PropTypes.bool.isRequired,
     user: PropTypes.object.isRequired,
+    repos: PropTypes.array.isRequired,
     getUser: PropTypes.func.isRequired,
+    getUserRepos: PropTypes.func.isRequired,
   }
 
   render() {
@@ -35,7 +39,7 @@ class User extends Component {
       hireable,
     } = this.props.user
 
-    const { loading } = this.props
+    const { loading, repos } = this.props
 
     if (loading) return <Spinner />
 
@@ -105,6 +109,10 @@ class User extends Component {
           <div className="badge badge-light">Public Repos: {public_repos}</div>
           <div className="badge badge-dark">Public Gists: {public_gists}</div>
         </div>
+        <Fragment>
+          <h3>Latest Repos: </h3>
+          <Repos repos={repos} />
+        </Fragment>
       </Fragment>
     )
   }
