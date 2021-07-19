1a8da8f56cb28cbc1c730373927d0a00c7372cba
Subham Bharadwaz <subhamsbharadwaz@gmail.com>
2021-07-19 10:51:27 +0530

    created User.js(mapped users and display them )

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
 import './App.css'
 
+// Import Components
+import Navbar from './components/layout/Navbar'
+import Users from './components/users/Users'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello World</h1>
+        <Navbar />
+        <div className="container">
+          <Users />
+        </div>
       </div>
     )
   }
